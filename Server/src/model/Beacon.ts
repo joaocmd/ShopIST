dffb7a2d140dcf90820a9b93df20ae5f0d2c4b01
joaocmd <joaodavid1999@hotmail.com>
2021-04-07 21:12:19 +0100

    Add beacon domain

---
+++ Server/src/model/Beacon.ts
@@ -0,0 +1,48 @@
+import Location from './Location'
+import regression, { DataPoint } from 'regression'
+
+type EnterRecord {
+    peopleAhead: number,
+    enterTime: Date
+}
+
+type RegressionData {
+    peopleAhead: number,
+    timeTaken: number
+}
+
+export default class {
+    uuid: string
+    location: Location
+
+    #currentPeople: Record<string, EnterRecord>
+    #regressionData: RegressionData[]
+
+    constructor(uuid: string, location: Location) {
+        this.uuid = uuid
+        this.location = location
+        this.#currentPeople = {}
+        this.#regressionData = []
+    }
+
+    addPerson(token: string) {
+        const record: EnterRecord = { peopleAhead: Object.keys(this.#currentPeople).length, enterTime: new Date() }
+        this.#currentPeople[token] = record
+    }
+
+    removePerson(token: string) {
+        const entrance = this.#currentPeople[token]
+        delete this.#currentPeople[token]
+        const record: RegressionData = { peopleAhead: entrance.peopleAhead, timeTaken: new Date().getTime() - entrance.enterTime.getTime() }
+        this.#regressionData.push(record)
+    }
+
+    estimateCurrentTime() {
+        const peopleInLine = Object.keys(this.#currentPeople).length
+        // converts to { peopleAhead: x, timeTaken: y} => [x, y]
+        const formattedData = this.#regressionData.map(r => [r.peopleAhead, r.timeTaken] as DataPoint)
+
+        const result = regression.linear(formattedData)
+        return result.predict(peopleInLine)
+    }
+}
--- Server/src/model/Location.ts
+++ Server/src/model/Location.ts
@@ -1,4 +1,18 @@
-export type Location = {
-	latitude: string,
-	longitude: string
+import { getDistance as distanceBetween } from 'geolib'
+
+export default class Location {
+	latitude: number
+	longitude: number
+
+	constructor(coords: { latitude: number, longitude: number }) {
+		this.latitude = coords.latitude
+		this.longitude = coords.longitude
+	}
+
+	getDistance(other: Location) {
+		return distanceBetween(
+			{ latitude: this.latitude, longitude: this.longitude },
+			{ latitude: other.latitude, longitude: other.longitude }
+		)
+	}
 }
