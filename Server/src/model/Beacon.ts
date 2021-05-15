d7fc22698f49f00c11f593f22e73bdbb0e09f3d2
joaocmd <joaodavid1999@hotmail.com>
2021-05-15 18:19:02 +0100

    Fix linearreg bug because 0 is falsy, use !== null

--- Server/src/model/Beacon.ts
+++ Server/src/model/Beacon.ts
@@ -15,8 +15,8 @@ export default class {
     uuid: string
     location: Location
 
-    #currentPeople: Record<string, EnterRecord> = {}
-    #regressionData: RegressionData[] = []
+    currentPeople: Record<string, EnterRecord> = {}
+    regressionData: RegressionData[] = []
 
     constructor(uuid: string, location: Location) {
         this.uuid = uuid
@@ -24,36 +24,32 @@ export default class {
     }
 
     addPerson(token: string, nrItems: number) {
-        if (!!this.#currentPeople[token]) {
+        if (!!this.currentPeople[token]) {
             throw 'person-already-in-beacon'
         }
         if (nrItems <= 0) {
             throw 'no-items'
         }
         const record: EnterRecord = { itemsAhead: this.itemsInLine() + nrItems, enterTime: new Date() }
-        this.#currentPeople[token] = record
+        this.currentPeople[token] = record
     }
 
     removePerson(token: string) {
-        if (!this.#currentPeople[token]) {
+        if (!this.currentPeople[token]) {
             throw 'person-not-in-beacon'
         }
-        const entrance = this.#currentPeople[token]
-        delete this.#currentPeople[token]
+        const entrance = this.currentPeople[token]
+        delete this.currentPeople[token]
         const record: RegressionData = { itemsAhead: entrance.itemsAhead, timeTaken: new Date().getTime() - entrance.enterTime.getTime() }
-        // only account for valid cases (person might leave the queue because she forgot an item)
-        if (record.timeTaken > 10000) {
-            this.#regressionData.push(record)
-        }
+        this.regressionData.push(record)
     }
 
     estimateCurrentTime(): number | null {
         // converts { peopleAhead: x, timeTaken: y} to [x, y]
-        // TODO: Check if it is calculating well
-        const formattedData = this.#regressionData.map(r => [r.itemsAhead, r.timeTaken] as DataPoint)
+        const formattedData = this.regressionData.map(r => [r.itemsAhead, r.timeTaken] as DataPoint)
 
         const result = regression.linear(formattedData)
-        if (result.equation[1] == NaN) {
+        if (this.regressionData.length <= 1) {
             return null
         } else {
             return Math.max(0, result.predict(this.itemsInLine())[1])
@@ -61,6 +57,6 @@ export default class {
     }
 
     itemsInLine(): number {
-        return Object.values(this.#currentPeople).map(r => r.itemsAhead).reduce((acc, val) => acc + val, 0)
+        return Object.values(this.currentPeople).map(r => r.itemsAhead).reduce((acc, val) => acc + val, 0)
     }
 }
